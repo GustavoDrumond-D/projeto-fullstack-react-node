ba9e03ef80f4e79553ca648de454e43aff93aa38
Gustavo Drumond <gdrumondduarte@gmail.com>
2024-10-24 18:33:01 -0300

    Styled Components

--- Projeto_Alura/alura-books/src/Components/IconesHeader/index.js
+++ Projeto_Alura/alura-books/src/Components/IconesHeader/index.js
@@ -1,19 +1,27 @@
-    import "./estilo.css"
-    import Perfil from "../../imagens/perfil.svg"
-    import Sacola from '../../imagens/sacola.svg'
+import Perfil from "../../imagens/perfil.svg"
+import Sacola from '../../imagens/sacola.svg'
+import styled from "styled-components"
+    
 
-    const icones = [Perfil,Sacola]
+const Icone = styled.li`
+    margin-right: 40px; /* Adiciona uma margem à direita de 40 pixels ao elemento com a classe .icone, criando espaço entre os ícones adjacentes. */
+    width: 25px; /* Define a largura do elemento .icone para 25 pixels. */
+`
+const Icones = styled.ul`
+    display: flex; /* Define o elemento .icones como um contêiner flexível, permitindo o alinhamento dos ícones em uma linha. */
+    align-items: center; /* Centraliza os ícones ao longo do eixo transversal (vertical) dentro do contêiner flexível. */
+`
 
-    function IconesHeader(){
-        return (
-            <ul className="icones">
-            { icones.map( (icone) => (
-            <li className="icone">
-                <img src={icone} alt="Icones"></img>
-            </li>
-            ) ) }
-        </ul>
-        )
-    }
+const icones = [Perfil,Sacola]
 
-    export default IconesHeader;
+function IconesHeader(){
+    return (
+        <Icones>
+        { icones.map( (icone) => (
+            <Icone><img src={icone} alt="Icones"></img></Icone>
+        ) ) }
+    </Icones>
+    )
+}
+
+export default IconesHeader;
--- Projeto_Alura/alura-books/src/Components/Logo/index.js
+++ Projeto_Alura/alura-books/src/Components/Logo/index.js
@@ -1,17 +1,21 @@
 import logo from '../../imagens/logo.svg'
-import './estilo.css'
+import styled from 'styled-components'
+
+const LogoContainer = styled.div`
+  display: flex; /* Define o elemento com a classe 'logo' como um contêiner flexível, permitindo o uso de layout flexbox para organizar seus filhos (itens contidos) de forma eficiente. */
+  font-size: 30px; /* Define o tamanho da fonte para 30 pixels, aplicando-se ao texto contido no elemento com a classe 'logo'. */
+`
+
+const LogoImage = styled.img`
+  margin-right: 10px; /* Adiciona uma margem à direita de 10 pixels ao elemento com a classe 'logo-img', criando espaço entre a imagem e o próximo elemento adjacente dentro do contêiner flexível. */
+`
 
 function Logo(){
     return (
-        <div className="logo">
-        <img src={logo} alt="Logo do Alura-Books" className='logo-img'></img>
-        <p>
-          <strong>
-            Alura
-          </strong>
-          Books
-        </p>
-      </div>
+        <LogoContainer>
+          <LogoImage src={logo} alt="Logo do Alura-Books" className='logo-img'></LogoImage>
+          <p><strong>Alura</strong>Books</p>
+      </LogoContainer>
     )
 };
 
