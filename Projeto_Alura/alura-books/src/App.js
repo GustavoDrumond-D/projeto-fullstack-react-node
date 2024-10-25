2c93653bc57ff1563d5b3b20dc342ecfc4875f2b
Gustavo Drumond <gdrumondduarte@gmail.com>
2024-10-24 21:29:07 -0300

    Estados

--- Projeto_Alura/alura-books/src/App.js
+++ Projeto_Alura/alura-books/src/App.js
@@ -1,18 +1,20 @@
 import Header from './Components/Header';
 import styled from 'styled-components';
+import Pesquisa from './Components/Pesquisa';
 
 const AppContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-image: linear-gradient(90deg, #002F52 35%, #326589);
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002F52 35%, #326589);
 `
 
 function App() {
-  return (
-    <AppContainer>
-      <Header />
-    </AppContainer>
-  );
+return (
+  <AppContainer>
+    <Header />
+    <Pesquisa />
+  </AppContainer>
+);
 }
 
 export default App;
