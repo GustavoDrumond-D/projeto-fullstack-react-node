097b3846967c5c8eb92a0be29cddbfd225218d71
Gustavo Drumond <gdrumondduarte@gmail.com>
2024-10-27 12:39:59 -0300

    JSON para armazenar dados

--- Projeto_Alura/alura-books-server/controladores/livro.js
+++ Projeto_Alura/alura-books-server/controladores/livro.js
@@ -1,6 +1,9 @@
+const {getTodosLivros} = require('../servicos/livros')
+
 function getLivros(req, res) {
     try{
-        res.send("Requisição do tipo GET")
+        const livros = getTodosLivros()
+        res.send(livros)
     } catch (error) {
         res.status(500).send(error.masage)
     }
